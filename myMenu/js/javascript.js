ebcaddd5bd1003dc23f400ccb108ab80596251e3
bonojeong <by1619@hanmail.net>
2021-06-17 13:55:55 +0900

    mymenu 업로드입니다.

---
+++ myMenu/js/javascript.js
@@ -0,0 +1,49 @@
+//a 태그 새로고침 방지
+$('a[href="#"]').click(function(e) {
+    e.preventDefault();
+});
+
+// 1. nav p 마우스 click 했을 때 classList 추가 .nav_depth_menu 왼쪽으로 이동
+function navOn() {
+    document.querySelector('.nav_depth_menu').classList.add('navToggle');
+}
+
+// 2. depth nav 생성
+$(document).ready(function(){
+    $('.nav > p').eq(0).click(function(){
+        $('.depth_text1').css("display","none");
+        $('.depth_text1').eq(0).css("display","block");
+    });
+    $('.nav > p').eq(1).click(function(){
+        $('.depth_text1').css("display","none");
+        $('.depth_text1').eq(1).css("display","block");
+    });
+    $('.nav > p').eq(2).click(function(){
+        $('.depth_text1').css("display","none");
+        $('.depth_text1').eq(2).css("display","block");
+    });
+    $('.nav > p').eq(3).click(function(){
+        $('.depth_text1').css("display","none");
+        $('.depth_text1').eq(3).css("display","block");
+    });
+    $('.nav > p').eq(4).click(function(){
+        $('.depth_text1').css("display","none");
+        $('.depth_text1').eq(4).css("display","block");
+    });
+    $('.nav > p').eq(5).click(function(){
+        $('.depth_text1').css("display","none");
+        $('.depth_text1').eq(5).css("display","block");
+    });
+    $('.nav > p').eq(6).click(function(){
+        $('.depth_text1').css("display","none");
+        $('.depth_text1').eq(6).css("display","block");
+    });
+    
+    // nav를 leave 했을 때 depth 메뉴 바깥으로
+    $('#menu').on('mouseleave',function(){
+        $('.nav_depth_menu').removeClass('navToggle');
+        $('.depth_text1').css("display","none");
+    })
+
+})
+
